21ad0c017b81b88513be59650dd29b6031f47ca7
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2009-02-08 22:12:13 +0000

    added bin migration Histo and (normalized) matrix

--- EffAcc/interface/EffHistos.h
+++ EffAcc/interface/EffHistos.h
@@ -14,12 +14,14 @@ class EffHistos {
 
  public:
   void Book(TFileDirectory& tfd);
-  void Fill(const ::math::PtEtaPhiMLorentzVector& e1, const ::math::PtEtaPhiMLorentzVector& e2);
+  void Fill(const ::math::PtEtaPhiMLorentzVector& e1, const ::math::PtEtaPhiMLorentzVector& e2, 
+            const ::math::PtEtaPhiMLorentzVector& eTL1, const ::math::PtEtaPhiMLorentzVector& eTL2); 
+  void WrapUp(void);
 
  private:
   TH1 *mZ_,*YZ_, *ptZ_;
   TH1 *e1eta_,*e2eta_,*e1phi_,*e2phi_,*e1pt_,*e2pt_;
-  TH2 *mZ_Y_,*mZ_pt_,*pt_Y_,*e1eta_YZ_,*e2eta_YZ_,*e1eta_ptZ_,*e2eta_ptZ_,*e1eta_e2eta_;
+  TH2 *mZ_Y_,*mZ_pt_,*pt_Y_,*e1eta_YZ_,*e2eta_YZ_,*e1eta_ptZ_,*e2eta_ptZ_,*e1eta_e2eta_,*YZTL_YZ_,*YZTL_YZ_matrix_;
 
 }; 
 
