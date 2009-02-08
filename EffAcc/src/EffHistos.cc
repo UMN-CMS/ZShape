beaa28ea6e34f1b70c20041284111375a1178df6
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2009-02-08 22:14:36 +0000

    added bin migration Histo and (normalized) matrix removed double filling of 2d histrograms pt axis range divided by 2 (400x2=800 GeV/c is too much)

--- EffAcc/src/EffHistos.cc
+++ EffAcc/src/EffHistos.cc
@@ -13,7 +13,7 @@ void EffHistos::Book(TFileDirectory& tdf) {
   float maxY     = 5.5;
   float minZmass = 50;
   float maxZmass = 140;
-  const int yBinsPerUnitFine = 10;
+  // const int yBinsPerUnitFine = 10;
   const int yBinsPerUnitCoarse = 4;
 
   mZ_  = tdf.make<TH1F>("Z0_mass","Z0_mass;m_{Z0} (GeV/c^{2})", 50, minZmass, maxZmass);
@@ -24,8 +24,8 @@ void EffHistos::Book(TFileDirectory& tdf) {
   e2eta_ = tdf.make<TH1F>("e2_eta","e2_eta;#eta_{e2}", 100, -5, 5);
   e1phi_ = tdf.make<TH1F>("e1_phi","e1_phi;#phi_{e1}", 100, -pi, pi);
   e2phi_ = tdf.make<TH1F>("e2_phi","e2_phi;#phi_{e2}", 100,  -pi, pi);
-  e1pt_  = tdf.make<TH1F>("e1_P_t","e1_P_t;p_{T,e1}", 200, 0, maxPt*2);
-  e2pt_  = tdf.make<TH1F>("e2_P_t","e2_P_t;p_{T,e2}", 200, 0, maxPt*2);
+  e1pt_  = tdf.make<TH1F>("e1_P_t","e1_P_t;p_{T,e1}", 200, 0, maxPt);
+  e2pt_  = tdf.make<TH1F>("e2_P_t","e2_P_t;p_{T,e2}", 200, 0, maxPt);
 
   //Now make the 2-D histograms
 
@@ -38,23 +38,32 @@ void EffHistos::Book(TFileDirectory& tdf) {
   e1eta_ptZ_   = tdf.make<TH2F>("e1_eta_vs_Ptz","e1_eta_vs_PtZ;#eta_{e1};p_{T,Z0}", 50, -5, 5,50, 0, maxPt );
   e2eta_ptZ_   = tdf.make<TH2F>("e2_eta_vs_Ptz","e2_eta_vs_PtZ;#eta_{e2};p_{T,Z0}", 50, -5, 5,50, 0, maxPt );
   e1eta_e2eta_ = tdf.make<TH2F>("e1_eta_vs_e2_eta","e1_eta_vs_e2_eta;#eta_{e1};#eta_{e2}", 50, -5, 5,50, -5, 5 );
+ 
+  YZTL_YZ_       = tdf.make<TH2F>("YZTL_vs_YZ","YZTL_vs_YZ;Y_{Z};Y_{ZtreeLevel}",int((maxY*2)*yBinsPerUnitCoarse),-maxY,maxY,int((maxY*2)*yBinsPerUnitCoarse),-maxY,maxY); 
+  YZTL_YZ_matrix_= tdf.make<TH2F>("YZTL_vs_YZ_matrix","YZTL_vs_YZ_matrix;Y_{Z};Y_{ZtreeLevel}",int((maxY*2)*yBinsPerUnitCoarse),-maxY,maxY,int((maxY*2)*yBinsPerUnitCoarse),-maxY,maxY); 
 
 }
 
-void EffHistos::Fill(const ::math::PtEtaPhiMLorentzVector& e1, const ::math::PtEtaPhiMLorentzVector& e2) {
+void EffHistos::Fill(const ::math::PtEtaPhiMLorentzVector& e1, const ::math::PtEtaPhiMLorentzVector& e2, 
+		     const ::math::PtEtaPhiMLorentzVector& eTL1, const ::math::PtEtaPhiMLorentzVector& eTL2) { 
 
   XYZTLorentzVector p1(e1);
   XYZTLorentzVector p2(e2);
+  XYZTLorentzVector pTL1(eTL1); 
+  XYZTLorentzVector pTL2(eTL2); 
 
   XYZTLorentzVector pZ = p1 + p2 ;
+  XYZTLorentzVector pTLZ = pTL1 + pTL2 ; 
 
   //  debug
-//  std::cout << "this is e1:"  << p1 << std::endl;
-//  std::cout << "this is e2:"  << p2 << std::endl;
-//  std::cout << "this is Z:"  << pZ << std::endl;
-//  float rapidity = 0.5 * log( ( pZ.E()+pZ.Pz() ) / ( pZ.E()-pZ.Pz() ) );  
-//  std::cout << "rapidity calculated: " << rapidity  << " while from method: " << pZ.Rapidity() << std::endl;
-  
+  //  std::cout << "this is e1:"  << p1 << std::endl;
+  //  std::cout << "this is e2:"  << p2 << std::endl;
+  //  std::cout << "this is Z:"  << pZ << std::endl;
+  //  float rapidity = 0.5 * log( ( pZ.E()+pZ.Pz() ) / ( pZ.E()-pZ.Pz() ) );  
+  //  std::cout << "rapidity calculated: " << rapidity  << " while from method: " << pZ.Rapidity() << std::endl;
+  //   rapidity = 0.5 * log( ( pTLZ.E()+pTLZ.Pz() ) / ( pTLZ.E()-pTLZ.Pz() ) );   
+  //   std::cout << "rapidity calculated: " << rapidity  << " while from method: " << pTLZ.Rapidity() << std::endl;
+
   float zMass = sqrt ( pZ.Dot(pZ) );
   float zY    = pZ.Rapidity();
   float zPt   = pZ.Pt();
@@ -67,13 +76,15 @@ void EffHistos::Fill(const ::math::PtEtaPhiMLorentzVector& e1, const ::math::PtE
   float e2phi = p2.Phi();
   float e2Pt  = p2.Pt();
 
+  float zTLY = pTLZ.Rapidity(); 
+
   mZ_  -> Fill(zMass);
   YZ_  -> Fill(zY);
   ptZ_ -> Fill(zPt);
 
-  mZ_Y_->Fill(zY,zMass);
-  mZ_pt_->Fill(zPt,zMass);
-  pt_Y_->Fill(zY,zPt);
+  //mZ_Y_->Fill(zY,zMass); // this was being filled twice! 
+  //mZ_pt_->Fill(zPt,zMass); // this was being filled twice! 
+  //pt_Y_->Fill(zY,zPt); // this was being filled twice! 
 
   e1eta_ -> Fill(e1eta);
   e1pt_  -> Fill(e1Pt);
@@ -95,4 +106,35 @@ void EffHistos::Fill(const ::math::PtEtaPhiMLorentzVector& e1, const ::math::PtE
   
   e1eta_e2eta_ -> Fill(e1eta,e2eta);
 
+  // tree level rapidity VS fast reco-ed rapidity 
+  YZTL_YZ_ -> Fill(zY,zTLY); 
 }
+
+
+void EffHistos::WrapUp(){ 
+ 
+  if( YZTL_YZ_==0 &&  YZTL_YZ_matrix_==0) return; 
+ 
+  int numBinX =  YZTL_YZ_ ->GetNbinsX(); 
+  int numBinY =  YZTL_YZ_ ->GetNbinsY(); 
+   
+  // filling migration matricex with by normalixing migration histograms 
+  float integral; 
+  for (int binY=1; binY<=numBinY; binY++) 
+    { 
+      integral=0; 
+      for (int binX=1; binX<=numBinX; binX++) 
+	{   integral += YZTL_YZ_->GetBinContent(binX,binY); } 
+     
+      if (integral==0) continue; 
+      for (int binX=1; binX<=numBinX; binX++) 
+	{   YZTL_YZ_matrix_->SetBinContent(binX,binY,   YZTL_YZ_->GetBinContent(binX,binY) / integral); } 
+ 
+    } 
+  
+  // debug 
+  //    std::cout << "\n num entries HIS: " << numBinX << " " << numBinY << " " << YZTL_YZ_->GetEntries() << std::endl; 
+  //    std::cout << "\n num entries MATR: " << YZTL_YZ_matrix_->GetNbinsX() << " " << YZTL_YZ_matrix_->GetNbinsY()  
+  //        << " " << YZTL_YZ_matrix_->GetEntries() << std::endl; 
+  
+} 
