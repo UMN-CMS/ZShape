cab7761acdd884b8343e35c813870d0139d2f4f9
mansj <>
2009-06-15 21:34:51 +0000

    Update to production version

--- Paper/root/plotFinal.C
+++ Paper/root/plotFinal.C
@@ -28,6 +28,7 @@ struct DataSeries {
     }
   }
   DataSeries(const char* fname, int iy=0);
+  DataSeries(int lumi, const char* fnameformat, int iy=0);
 
   TGraphErrors* makeTGE(int bins, int startFrom) {
     return new TGraphErrors(bins,xave+startFrom,y+startFrom,xwidth+startFrom,ey+startFrom);
@@ -41,10 +42,13 @@ struct DataSeries {
     for (int i=1; i<=bins; i++) {
       hist->SetBinContent(i,y[startFrom+i-1]);
       hist->SetBinError(i,ey[startFrom+i-1]);
+      printf("%s %d %f %f \n",name,i+startFrom,y[startFrom+i-1],ey[startFrom+i-1]);
     }
     hist->SetDirectory(0);
     return hist;
   }
+
+  void create(const char* file, int iy);
 
   double xave[44];
   double xwidth[44];
@@ -56,7 +60,15 @@ struct DataSeries {
 #include "tdrstyle.C"
 #include "zrapidityStandard.C"
 
+DataSeries::DataSeries(int lumi, const char* file, int iy) {
+  char fullname[1024];
+  sprintf(fullname,file,lumi);
+  create(fullname,iy);
+}
 DataSeries::DataSeries(const char* file, int iy) {
+  create(file,iy);
+}
+void DataSeries::create(const char* file, int iy) {
   clear();
 
   FILE* f=fopen(file,"r");
@@ -78,7 +90,7 @@ DataSeries::DataSeries(const char* file, int iy) {
     atetotal+=ate;
     for (int iq=0; iq<=iy && found!=0; iq++) {
       found=sscanf(line+atetotal," %f %n ",&q[iq],&ate);
-      printf("%d %d %d %f\n",i,iq,ate,q[iq]);
+      //      printf("%d %d %d %f\n",i,iq,ate,q[iq]);
       atetotal+=ate;
     }
     if (found!=1) continue;
@@ -90,16 +102,16 @@ DataSeries::DataSeries(const char* file, int iy) {
   fclose(f);
 }
 
-void plotFinal() {
+void plotFinal(int lumi=100) {
   setTDRStyle();
-  TFile mctruth("histo_40M.root");
+  TFile mctruth("zshape_30M.root");
 
   TH1* truth=(TH1*)(mctruth.Get("mcEff/All/Z0_Y")->Clone("truth"));
   truth->SetDirectory(0);
   
-  DataSeries data_ee("Tight-ECAL-Loose-ECAL.txt"), data_ef("Tight-ECAL-HF.txt"), data_all;
+  DataSeries data_ee(lumi,"Tight-ECAL-Loose-ECAL_%d.txt"), data_ef(lumi,"Tight-ECAL-HF_%d.txt"), data_all;
   
-  DataSeries effAcc("effacc.txt"), ea_statErr("staterror_100pb.txt");
+  DataSeries effAcc("effacc.txt"), ea_statErr(lumi,"StatError_%dpb.txt");
   DataSeries binMig("BinMigrationEffect.txt");
   DataSeries binMigPos(binMig);
 
@@ -119,8 +131,10 @@ void plotFinal() {
 
   DataSeries dataStatError(data_all);
 
-  DataSeries backgroundExp("background.txt",0);
-  DataSeries backgroundDelta("background.txt",0);
+
+  DataSeries backgroundExp(lumi,"background_%d.txt",0);
+  DataSeries backgroundAct(lumi,"background_%d.txt",2);
+  DataSeries backgroundExpErr(lumi,"background_%d.txt",1);
   DataSeries backgroundUncFrac(data_all);
 
   DataSeries pdfPos("pdfErrsPos.txt");
@@ -128,17 +142,22 @@ void plotFinal() {
   DataSeries pdfTotal(data_all);
   DataSeries pdfFrac(data_all);
 
-  TRandom workRand(50);
+  DataSeries pdfSens25("relativeSensitivity_cteq65_vector_25.txt");
+  //  DataSeries pdfSens16("relativeSensitivity_cteq65_vector_16.txt");
 
 
-  backgroundExp.y[8-1]=workRand.Poisson(backgroundExp.y[10-1]);
-  backgroundExp.y[9-1]=workRand.Poisson(backgroundExp.y[10-1]);
-  backgroundExp.y[36-1]=workRand.Poisson(backgroundExp.y[35-1]);
-  backgroundExp.y[37-1]=workRand.Poisson(backgroundExp.y[35-1]); 
+  //  TRandom workRand(50);
+
+  //  backgroundExp.y[8-1]=workRand.Poisson(backgroundExp.y[9-1]);
+  //  backgroundExp.y[9-1]=workRand.Poisson(backgroundExp.y[10-1]);
+  //  backgroundExp.y[36-1]=workRand.Poisson(backgroundExp.y[35-1]);
+  //  backgroundExp.y[37-1]=workRand.Poisson(backgroundExp.y[36-1]); 
 
   for (int i=firsti-1; i<lasti; i++) {
-    backgroundDelta.y[i]=workRand.Poisson(backgroundDelta.y[i])-backgroundDelta.y[i];
+    backgroundExpErr.y[i]=std::max(backgroundExpErr.y[i],0.003*data_all.y[i]);
   }
+  //    backgroundDelta.y[i]=workRand.Poisson(backgroundDelta.y[i])-backgroundDelta.y[i];
+  //  }
 
   DataSeries corrData(data_all);
   DataSeries corrDataSyst(data_all);
@@ -163,13 +182,13 @@ void plotFinal() {
 
     data_bkgd.y[i]+=backgroundExp.y[i];
 
-    backgroundUncFrac.y[i]=sqrt(backgroundExp.y[i])/data_bkgd.y[i];
+    backgroundUncFrac.y[i]=backgroundExpErr.y[i]/std::max(1.0,data_bkgd.y[i]);
 
-    corrDataBkgd.y[i]+=backgroundDelta.y[i];
+    corrDataBkgd.y[i]+=backgroundAct.y[i]-backgroundExp.y[i];
     corrDataBkgd.y[i]/=effAcc.y[i];
     corrDataBkgd.ey[i]=sqrt(data_all.y[i]+backgroundExp.y[i])/effAcc.y[i];
 
-    dataStatError.y[i]= corrDataBkgd.ey[i]/corrDataBkgd.y[i];
+    dataStatError.y[i]= corrDataBkgd.ey[i]/std::max(1.0,corrDataBkgd.y[i]);
 
     corrDataSyst.y[i]/=effAcc.y[i];
 
@@ -181,18 +200,18 @@ void plotFinal() {
     printf("%d %f %f %f %f\n",i,corrData.ey[i],corrData.y[i]*ea_statErr.y[i],errmig,pdfTotal.y[i]);
     corrDataSyst.ey[i]=sqrt(pow(corrDataBkgd.ey[i],2)+
 			    pow(corrDataBkgd.y[i]*ea_statErr.y[i],2)+
-			    fabs(backgroundExp.y[i])/effAcc.y[i]+
+			    pow(backgroundExpErr.y[i]/effAcc.y[i],2)+
 			    pow(errmig,2)+
 			    pow(pdfTotal.y[i],2)
 			    );
 
     totalSyst.ey[i]=sqrt( pow(corrDataBkgd.y[i]*ea_statErr.y[i],2)+
 			  pow(errmig,2)+
-			  fabs(backgroundExp.y[i])/effAcc.y[i]+
+			  pow(backgroundExpErr.y[i]/effAcc.y[i],2)+
 			  pow(pdfTotal.y[i],2)
 			  );
 
-    totalSyst.y[i]=totalSyst.ey[i]/corrDataBkgd.y[i];
+    totalSyst.y[i]=totalSyst.ey[i]/std::max(1.0,corrDataBkgd.y[i]);
 
     sumtotal+=corrDataSyst.y[i];
   }
@@ -200,16 +219,21 @@ void plotFinal() {
 
   DataSeries corrDataFold(corrData),corrDataSystFold(corrData);
 
-  FILE* ftable=fopen("ZRapidity_final_fold_table.tex","w");
+  char fnamework[1024];
+  sprintf(fnamework,"ZRapidity_final_fold_table-%d.tex",lumi);
+
+  FILE* ftable=fopen(fnamework,"w");
 
   fprintf(ftable,"\\begin{tabular}{|c|c||c|c|c|}\n\\hline\n");
   fprintf(ftable,"$|Y_{min}|$ & $|Y_{max}|$ & Measurement & Statistical Error & Systematic Error \\\\ \\hline\n");
   
   const int izero=23;
+  const double binwidth=0.25;
+  //  double binwidth=1.0;
   for (int i=0; i<=(lasti-firsti)/2; i++) {
     int j=izero+i-1;
     int jadd=izero-i-2;
-    corrDataFold.y[j]=(corrDataBkgd.y[j]+corrDataBkgd.y[jadd])/sumtotal;
+    corrDataFold.y[j]=(corrDataBkgd.y[j]+corrDataBkgd.y[jadd])/std::max(1.0,sumtotal);
 
 
     //    printf("%d %d (%f) %d (%f) \n",i,j,corrDataBkgd.y[j],jadd,corrDataBkgd.y[jadd]);
@@ -218,7 +242,7 @@ void plotFinal() {
 
     double errmig=(fabs(binMig.y[j])*corrData.y[j]+fabs(binMig.y[jadd])*corrData.y[jadd])/sumtotal;
     double pdferr=(pdfFrac.y[j]+pdfFrac.y[jadd])/2*corrDataFold.y[j]/sumtotal;
-    double bkgderr=sqrt(backgroundExp.y[j]+backgroundExp.y[jadd])/sumtotal;
+    double bkgderr=sqrt(pow(backgroundExpErr.y[j],2)+pow(backgroundExpErr.y[jadd],2))/ea_Ave/sumtotal;
 
     corrDataFold.ey[j]=sqrt(data_all.y[j]+data_all.y[jadd]+backgroundExp.y[j]+backgroundExp.y[jadd])/ea_Ave/sumtotal;
 
@@ -229,6 +253,12 @@ void plotFinal() {
 				pow(pdferr,2)+
 				pow(bkgderr,2)
 				);
+
+    corrDataFold.y[j]/=binwidth;
+    corrDataFold.ey[j]/=binwidth;
+    corrDataSystFold.y[j]/=binwidth;
+    corrDataSystFold.ey[j]/=binwidth;
+
 
     fprintf(ftable,"%7.2f & %7.2f & %7.5f & %7.5f & %7.5f \\\\ \n",
 	    corrDataFold.xave[j]-corrDataFold.xwidth[j],
@@ -242,7 +272,7 @@ void plotFinal() {
   fprintf(ftable,"\\hline\n\\end{tabular}");
   fclose(ftable);
 
-  truth->Scale(td/tt);
+  truth->Scale(td/tt*1.0);
 
   TGraph* rawd=data_all.makeTG(npoint,firsti-1);
   TGraph* rawdb=data_bkgd.makeTG(npoint,firsti-1);
@@ -252,7 +282,7 @@ void plotFinal() {
   // this is special
   TGraph* corrdsysb=new TGraphErrors(npoint,corrDataBkgd.xave+firsti-1,corrDataBkgd.y+firsti-1,corrDataSyst.xwidth+firsti-1,corrDataSyst.ey+firsti-1);
 
-  TH2* dummy=new TH2F("dummy","dummy",20,-3.75,3.75,30,0,5500);
+  TH2* dummy=new TH2F("dummy","dummy",20,-3.75,3.75,30,0,5800*lumi/100);
 
   dummy->GetYaxis()->SetTitle("Events/0.25 Units of Rapidity");
   dummy->GetXaxis()->SetTitle("Y_{Z}");
@@ -288,19 +318,36 @@ void plotFinal() {
   tl->AddEntry(truth,"CTEQ6.1 Prediction","l");
   tl->Draw();
 
-  zrap_Prelim(0.85,0.89,0.85,0.85);
+  zrap_Prelim(0.85,0.90,0.85,0.85);
+  zrap_Lumi(0.85,0.86,lumi);
 
-  c1->Print("ZRapidity_final.eps");
-  c1->Print("ZRapidity_final.png");
+  sprintf(fnamework,"ZRapidity_final-%d.eps",lumi);
+  c1->Print(fnamework);
+  sprintf(fnamework,"ZRapidity_final-%d.png",lumi);
+  c1->Print(fnamework);
 
   TCanvas* c2=new TCanvas("finalZRapFold","finalZRapFold",800,600);
-  TH2* dummy2=new TH2F("dummy2","dummy2",10,0.0,3.75,30,0,0.1);
+  TH2* dummy2=new TH2F("dummy2","dummy2",10,0.0,3.75,30,0,0.4);
   dummy2->SetDirectory(0);
 
   TH1* truth2=(TH1*)(truth->Clone("truth2"));
+  TH1* truthalt=(TH1*)(truth->Clone("truthalt"));
   truth2->SetDirectory(0);
-  for (int i=izero; i<=lasti; i++) 
-    truth2->SetBinContent(i,(truth2->GetBinContent(i)+truth2->GetBinContent(44-i+1))/sumtotal);
+  truthalt->SetDirectory(0);
+
+  double chi2_0=0;
+  double chi2_1=0;
+  double fudge=1.00;
+  for (int i=izero; i<=lasti; i++) {
+    truth2->SetBinContent(i,(truth2->GetBinContent(i)+truth2->GetBinContent(44-i+1))/sumtotal/binwidth*fudge);
+    double factor=1+(pdfSens25.y[i-1]+pdfSens25.y[44-i])/2.0;
+    truthalt->SetBinContent(i,truth2->GetBinContent(i)*factor);
+    if (corrDataSystFold.ey[i-1]<=0) continue;
+    printf("AltUniv: %d %f %f\n",i,factor,truthalt->GetBinContent(i));
+    chi2_0+=pow(truth2->GetBinContent(i)-corrDataFold.y[i-1],2)/pow(corrDataSystFold.ey[i-1],2);
+    chi2_1+=pow(truthalt->GetBinContent(i)-corrDataFold.y[i-1],2)/pow(corrDataSystFold.ey[i-1],2);
+  }
+
 
   c2->cd();
   dummy2->GetYaxis()->SetTitle("1/#sigma d#sigma/dY");
@@ -316,12 +363,17 @@ void plotFinal() {
   corrdfoldsys->Draw("PSAME");
 
   truth2->Draw("SAMEHIST");
+  truthalt->SetLineColor(kRed);
+  truthalt->Draw("SAMEHIST");
 
   zrap_Prelim(0.3,0.24,0.3,0.2);
+  zrap_Lumi(0.30,0.19,lumi);
+  
 
-  c2->Print("ZRapidity_final_fold.eps");
-  c2->Print("ZRapidity_final_fold.png");
-
+  sprintf(fnamework,"ZRapidity_final_fold-%d.eps",lumi);
+  c2->Print(fnamework);
+  sprintf(fnamework,"ZRapidity_final_fold-%d.png",lumi);
+  c2->Print(fnamework);
 
   TCanvas* c3=new TCanvas("finalZRapErrors","finalZRapErrors",800,600);
   TH1* dummy3=new TH1F("dummy3","dummy3",10,-3.76,3.75);
@@ -368,7 +420,7 @@ void plotFinal() {
   gr_pdf->Draw("HSAME");
   gr_systotal->Draw("HISTSAME");
 
-  TLegend* tl=new TLegend(0.35,0.70,0.75,0.94,"Error Source Contributions");
+   tl=new TLegend(0.35,0.70,0.75,0.94,"Error Source Contributions");
   tl->AddEntry(binmiggr,"Bin Migration","L");
   tl->AddEntry(effacc_stat_gr,"Efficiency Statistics","L");
   tl->AddEntry(gr_pdf,"PDF (#epsilon #times A) Uncertainty","L");
@@ -378,8 +430,13 @@ void plotFinal() {
   tl->Draw();
   
   zrap_Prelim(0.3,0.24,0.3,0.2);
+  zrap_Lumi(0.30,0.19,lumi);
 
-  c3->Print("ZRapidity_final_errors.eps");
-  c3->Print("ZRapidity_final_errors.png");
+  sprintf(fnamework,"ZRapidity_final_errors-%d.eps",lumi);
+  c3->Print(fnamework);
+  sprintf(fnamework,"ZRapidity_final_errors-%d.png",lumi);
+  c3->Print(fnamework);
+
+  printf(" Two models: %f %f \n",chi2_0,chi2_1);
 
 }
