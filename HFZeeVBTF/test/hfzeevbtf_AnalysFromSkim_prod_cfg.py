077e3ca7965561e968a52544c7e1825491c0321b
franzoni <>
2010-11-15 16:27:57 +0000

    - use input from .py tag to chose collection of Hfelectrons to use - allow for tunable HF cuts: iso&var2d or otherwrise single componets of var2d   (geared towards eSeL studies, where one needs an instance of the analyzer w/o any selection on eSeL)

--- HFZeeVBTF/test/hfzeevbtf_AnalysFromSkim_prod_cfg.py
+++ HFZeeVBTF/test/hfzeevbtf_AnalysFromSkim_prod_cfg.py
@@ -6,7 +6,7 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 2000
 
 process.maxEvents = cms.untracked.PSet( 
-	 input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(-1)
 )
 
 process.options = cms.untracked.PSet(
@@ -15,7 +15,7 @@ process.options = cms.untracked.PSet(
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
-    fileNames=cms.untracked.vstring(     )
+    fileNames=cms.untracked.vstring( )
 )
 
 
@@ -43,7 +43,8 @@ process.load("RecoEgamma.EgammaHFProducers.hfRecoEcalCandidate_cfi")
 process.hfRecoEcalCandidate.intercept2DCut=0.3
 process.hfRecoEcalCandidate.e9e25Cut      =0.94
 # 0.94 is the same as default in the HF cluster producer
-
+# 0: correction is off; 1: correction is activated
+process.hfRecoEcalCandidate.correctionType=0
 
 process.TFileService = cms.Service("TFileService",
        fileName = cms.string('')
@@ -92,13 +93,31 @@ process.Rej.myName              = cms.string('HFZeeVBTF-pincoPonco-Rej')
 process.Rej.acceptedElectronIDs = cms.vint32( 4, 5, 6, 7 )
 
 
+from RecoEgamma.EgammaHFProducers.hfRecoEcalCandidate_cfi import *
+process.hfRecoEcalCandidateLoose            = hfRecoEcalCandidate.clone()
+process.hfRecoEcalCandidateLoose.hfclusters = cms.InputTag("hfEMClusters")
+process.hfRecoEcalCandidateLoose.intercept2DCut=-100# this is to avoid JUST completely the usage of the esel
+process.hfRecoEcalCandidateLoose.e9e25Cut      =0.94
+# 0: correction is off; 1: correction is activated
+process.hfRecoEcalCandidateLoose.correctionType=0
+
+process.IdIsoRejHFIsoOnly                     = demo.clone()
+process.IdIsoRejHFIsoOnly.myName              = cms.string('HFZeeVBTF-IdIsoRejHFIsonly')
+process.IdIsoRejHFIsoOnly.acceptedElectronIDs = cms.vint32( 7 )
+# the following params are, respectively: e9e25_loose, e9e25_tight,  var2d_loose, var2d_tight,  eCOREe9_loose, eCOREe9_tight,  eSeL_loose, eSeL_tight;
+process.IdIsoRejHFIsoOnly.hFselParams         = cms.vdouble(0.90, 0.94,      -9999, -9999,    0.7, 0.85,     9999, 9999)
+process.IdIsoRejHFIsoOnly.DoLog               = cms.bool(True)
+process.IdIsoRejHFIsoOnly.hFElectrons         = cms.InputTag("hfRecoEcalCandidateLoose","","hfzeevbtfAnalysFromSkim")
+
+
 #  this module is run at the beginning of the job sequence just to count the number of events
 #  the analysis has been running on
 process.demoBefCuts                     = demo.clone()
 process.demoBefCuts.myName              = cms.string('demoBefCuts')
 process.demoBefCuts.acceptedElectronIDs = cms.vint32( 7 )
 process.demoBefCuts.DoLog               = cms.bool(False)
-
+#process.demoBefCuts.hFElectrons         = cms.InputTag("hfRecoEcalCandidateLoose","","hfzeevbtfAnalysFromSkim")
+process.demoBefCuts.hFElectrons   = cms.InputTag("hfRecoEcalCandidate","","")
 
 from ZShape.HFZeeVBTF.elewithmet_cfi import *
 #myDesiredMetCollection = "caloMET"
@@ -193,16 +212,18 @@ import FWCore.Modules.printContent_cfi
 process.dumpEv = FWCore.Modules.printContent_cfi.printContent.clone()
 
 process.p = cms.Path(
-    process.demoBefCuts # this one is just to count events (needed in MC!)
       # process.EG_1e28 *
-    *process.hfRecoEcalCandidate
-    *process.IdIso
-    *process.IdRej
-    *process.IsoRej
-    *process.Id
-    *process.Iso
-    *process.Rej
-    *process.IdIsoRej
+    process.hfRecoEcalCandidate
+    * process.hfRecoEcalCandidateLoose
+    * process.demoBefCuts # this one is just to count events (needed in MC!)
+    * process.IdIso
+    * process.IdRej
+    * process.IsoRej
+    * process.Id
+    * process.Iso
+    * process.Rej
+    * process.IdIsoRej
+    * process.IdIsoRejHFIsoOnly
     * process.metEleIdIsoRej
     * process.metEleIdIso
     * process.metEleIdRej
@@ -210,5 +231,7 @@ process.p = cms.Path(
     * process.metEleId
     * process.metEleIso
     * process.metEleRej
-    # *process.dumpEv
+    #* process.dumpEv
     )
+
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True))
