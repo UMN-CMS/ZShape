320feca80b032fcddf2525543b8765b369b7a80d
Jason Haupt <haupt@physics.umn.edu>
2010-12-16 03:52:29 +0000

    jason's 35 inv-pb updates 38X

--- EffAcc/python/SmearedElectronsProducer_cfi.py
+++ EffAcc/python/SmearedElectronsProducer_cfi.py
@@ -18,20 +18,20 @@ SmearedElectronsProducer = cms.EDProducer("ZSmearingProducer",
     ),
     EB = cms.PSet(
        #tuned by j. Mans on 06-08-09
-       p0 = cms.double(0.0282444) , 
+       p0 = cms.double(0.0379087) , 
        p1 = cms.double(1.06),
        p2 = cms.double(4.78),
-       c = cms.double(0.013497), #0.004
+       c = cms.double(0.009), #0.004
        alpha = cms.double(1.03),
-       mean = cms.double(0.998054), #back to 1.0 from 0.983
+       mean = cms.double(0.996621), #back to 1.0 from 0.983
        n = cms.double(3.0) 	   
     ),
      EE = cms.PSet(
-       p0 = cms.double(2.1),
+       p0 = cms.double(2.33236),
        p1 = cms.double(0.72),
        p2 = cms.double(0.15), #0.15
-       alpha = cms.double(1.90),
-       mean = cms.double(0.957862),
+       alpha = cms.double(1.89919),
+       mean = cms.double(0.954078),
        n = cms.double(2.0) 	
     )
                                           
