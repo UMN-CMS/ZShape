277b7db40da513f2b5ff7b251cc8cde77cc1fd49
mansj <>
2011-01-12 17:07:51 +0000

    Add HF energy scale

--- Base/src/Systematics.cc
+++ Base/src/Systematics.cc
@@ -4,7 +4,7 @@
 namespace zshape {
   
 
-  EnergyScale::EnergyScale(bool positive) : upshift_(positive) {
+  EnergyScale::EnergyScale(int ecal, int HF) : shiftECAL_(ecal), shiftHF_(HF) {
   }
    
   void EnergyScale::rescale(ZShapeElectron& electron) {
@@ -12,14 +12,14 @@ namespace zshape {
     double scale=1.0;
 
     if (accept.isInAcceptance(electron,EtaAcceptance::zone_EB)) {
-      if (upshift_) scale=1.01;
-      else scale=0.99;
+      if (shiftECAL_>0) scale=1.01;
+      else if (shiftECAL_<0) scale=0.99;
     } else if (accept.isInAcceptance(electron,EtaAcceptance::zone_EE)) {
-      if (upshift_) scale=1.03;
-      else scale=0.97;     
+      if (shiftECAL_>0) scale=1.03;
+      else if (shiftECAL_<0) scale=0.97;     
     } else if (accept.isInAcceptance(electron,EtaAcceptance::zone_HF)) {
-      if (upshift_) scale=1.00; // for now!
-      else scale=1.00; // for now!
+      if (shiftHF_>0) scale=1.10; 
+      else if (shiftHF_<0) scale=0.90; 
     }
 
     electron.p4_= math::PtEtaPhiMLorentzVector(electron.p4_.Pt()*scale,
