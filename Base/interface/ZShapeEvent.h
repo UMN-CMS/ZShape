1e143d60aaa31b6f63d25f60832a246e6fb0dd81
Jason Haupt <haupt@physics.umn.edu>
2011-01-15 07:53:17 +0000

    updating the base directory

--- Base/interface/ZShapeEvent.h
+++ Base/interface/ZShapeEvent.h
@@ -8,10 +8,14 @@
 #include <string>
 #include <ostream>
 
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+
+
 /** \class ZShapeEvent
   *  
-  * $Date: 2009/05/20 22:39:05 $
-  * $Revision: 1.5 $
+  * $Date: 2009/06/08 19:50:56 $
+  * $Revision: 1.6 $
   * \author J. Mans - Minnesota
   */
 class ZShapeEvent {
@@ -25,11 +29,20 @@ public:
 
   double m() const { return mass_; }
   double Y() const { return rap_; }
+  float EvtPVz() const { return mPVz_; }
+  float EvtBSz() const { return mBSz_; }
+  float EvtMET() const { return mMET_; }
+  float EvtTCMET() const { return mtcMET_; }
+  float EvtPFMET() const { return mpfMET_; }
+
 
   ZShapeElectron& elec(int i) { return ((i==0)?(*e1_):(*e2_)); }
   const ZShapeElectron& elec(int i) const { return ((i==0)?(*e1_):(*e2_)); }
   reco::GenParticle& elecTreeLevel(int i) { return ((i==0)?(*eTL1_):(*eTL2_)); }
   const reco::GenParticle& elecTreeLevel(int i) const { return ((i==0)?(*eTL1_):(*eTL2_)); }
+  
+  //This loads the Extra event information
+  void initEvent(const edm::Event& iEvent, const edm::EventSetup& iSetup);
 
   virtual ~ZShapeEvent();
   virtual void clear();
@@ -44,6 +57,12 @@ private:
   reco::GenParticle * eTL1_;
   reco::GenParticle * eTL2_;
   double mass_, rap_;
+  //These EXTRA Event Variables are mutable as they are filled via a const method. Sign=Significance
+  mutable uint32_t run_, lumi_, event_, mNPV_;
+  mutable float mPVx_,mPVy_,mPVz_,mBSx_,mBSy_,mBSz_; 
+  mutable float mMET_,mSumET_,mMETSign_,mtcMET_,mtcSumET_,
+	        mtcMETSign_,mpfMET_,mpfSumET_,mpfMETSign_;
+
 };
 
 #endif
