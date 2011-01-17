164a4a3eac48bd291a50ca3b7116bdcc98bf5ed2
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2011-01-17 18:50:07 +0000

    eta-dependent scale variation (transparency) -  +- 0.2% along EB, linearly with |eta| -  an extra +- 1.5% along EE, linearly with |eta|

--- Base/src/Systematics.cc
+++ Base/src/Systematics.cc
@@ -8,7 +8,7 @@ namespace zshape {
   }
   // the sign of ecal and HF determine whether scale variation is up or down
   // the scale variation is global (isEtaDep=false) or eta-dependent (isEtaDep=true); always global for HF
-  // eta-dependent: 0.4% linear with eta along EB, 3% further along TK-covered-EE
+  // eta-dependent: 0.2% linear with eta along EB, 1.5% further along TK-covered-EE
 
   void EnergyScale::rescale(ZShapeElectron& electron) {
     EtaAcceptance accept;
@@ -16,11 +16,11 @@ namespace zshape {
     double absEta=std::fabs(electron.detEta_);
 
     if (accept.isInAcceptance(electron,EtaAcceptance::zone_EB)) {
-      if (shiftECAL_>0)         isEtaDep_   ?   (scale=1+0.004*absEta/1.50)   : (scale=1.01);
-      else if (shiftECAL_<0)    isEtaDep_   ?   (scale=1-0.004*absEta/1.50)   : (scale=0.99);
+      if (shiftECAL_>0)         isEtaDep_   ?   (scale=1+0.002*absEta/1.50)   : (scale=1.01);
+      else if (shiftECAL_<0)    isEtaDep_   ?   (scale=1-0.002*absEta/1.50)   : (scale=0.99);
     } else if (accept.isInAcceptance(electron,EtaAcceptance::zone_EE)) {
-      if (shiftECAL_>0)         isEtaDep_   ?   (scale=1.004+0.03*(absEta-1.50))   : (scale=1.03);
-      else if (shiftECAL_<0)    isEtaDep_   ?   (scale=0.996-0.03*(absEta-1.50))   : (scale=0.97);
+      if (shiftECAL_>0)         isEtaDep_   ?   (scale=1.002+0.015*(absEta-1.50))   : (scale=1.03);
+      else if (shiftECAL_<0)    isEtaDep_   ?   (scale=0.998-0.015*(absEta-1.50))   : (scale=0.97);
     } else if (accept.isInAcceptance(electron,EtaAcceptance::zone_HF)) {
       if (shiftHF_>0) scale=1.10; 
       else if (shiftHF_<0) scale=0.90; 
