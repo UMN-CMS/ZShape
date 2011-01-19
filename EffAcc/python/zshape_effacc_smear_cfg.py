a45722c261ad6c62e0e4cb9d0c382ae37ceb8b44
franzoni <>
2011-01-19 09:17:12 +0000

    for V00-05-05 studies

--- EffAcc/python/zshape_effacc_smear_cfg.py
+++ EffAcc/python/zshape_effacc_smear_cfg.py
@@ -14,7 +14,7 @@ process.maxEvents = cms.untracked.PSet(
     )
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring("" )
+    fileNames = cms.untracked.vstring("")
 )
 
 process.TFileService = cms.Service("TFileService",
@@ -26,67 +26,36 @@ process.load("ZShape.EffAcc.ZEfficiencyStd_cfi")
 
 # errors used are associated to the parameter vaues of SmearedElectronsProducer_cfi.py V00-05-03 :
 #a-EB-down
-#process.SmearedElectronsProducer.EB.p0 = 0.04143
+#process.SmearedElectronsProducer.EB.p0 = 0.0389
 #a-EB-up
-#process.SmearedElectronsProducer.EB.p0 = 0.04197
+#process.SmearedElectronsProducer.EB.p0 = 0.0431
 #a-EE-down
-#process.SmearedElectronsProducer.EE.p0 = 1.623
+#process.SmearedElectronsProducer.EE.p0 = 1.453
 #a-EE-up
-#process.SmearedElectronsProducer.EE.p0 = 1.629
-#a-HF-down
-#process.SmearedElectronsProducer.HF.stocastic = 3.432
-#a-HF-up
-#process.SmearedElectronsProducer.HF.stocastic = 3.48
+#process.SmearedElectronsProducer.EE.p0 = 1.593
 #alpha-EB-down
-#process.SmearedElectronsProducer.EB.alpha = 1.37853
+#process.SmearedElectronsProducer.EB.alpha = 1.373
 #alpha-EB-up
-#process.SmearedElectronsProducer.EB.alpha = 1.37947
+#process.SmearedElectronsProducer.EB.alpha = 1.429
 #alpha-EE-down
-#process.SmearedElectronsProducer.EE.alpha = 1.607
+#process.SmearedElectronsProducer.EE.alpha = 1.616
 #alpha-EE-up
-#process.SmearedElectronsProducer.EE.alpha = 1.623
+#process.SmearedElectronsProducer.EE.alpha = 1.738
 #c-EB-down
-#process.SmearedElectronsProducer.EB.c = 0.0088
+#process.SmearedElectronsProducer.EB.c = 0.0081
 #c-EB-up
-#process.SmearedElectronsProducer.EB.c = 0.0092
-#c-HF-down
-#process.SmearedElectronsProducer.HF.constant = 0.086
-#c-HF-up
-#process.SmearedElectronsProducer.HF.constant = 0.088
-
-
-# ad-hoc assumption of a +-5% variation on each parameter 
-#a-EB-Vdown
-#process.SmearedElectronsProducer.EB.p0 = 0.0437332
-#a-EB-Vup
-#process.SmearedElectronsProducer.EB.p0 = 0.0395682
-#a-EE-Vdown
-#process.SmearedElectronsProducer.EE.p0 = 1.6224
-#a-EE-Vup
-#process.SmearedElectronsProducer.EE.p0 = 1.70779
-#a-HF-Vdown
-#process.SmearedElectronsProducer.HF.stocastic = 3.44762
-#a-HF-Vup
-#process.SmearedElectronsProducer.HF.stocastic = 3.62907
-#alpha-EB-Vdown
-#process.SmearedElectronsProducer.EB.alpha = 1.31005
-#alpha-EB-Vup
-#process.SmearedElectronsProducer.EB.alpha = 1.44795
-#alpha-EE-Vdown
-#process.SmearedElectronsProducer.EE.alpha = 1.53452
-#alpha-EE-Vup
-#process.SmearedElectronsProducer.EE.alpha = 1.69604
-#c-EB-Vdown
-#process.SmearedElectronsProducer.EB.c = 0.00855
-#c-EB-Vup
-#process.SmearedElectronsProducer.EB.c = 0.00945
-#c-HF-Vdown
-#process.SmearedElectronsProducer.HF.constant = 0.0821845
-#c-HF-Vup
-#process.SmearedElectronsProducer.HF.constant = 0.0908355
-
+#process.SmearedElectronsProducer.EB.c = 0.0099
+#c-HFm-down
+#process.SmearedElectronsProducer.HF.constantm = 0.128
+#c-HFm-up
+#process.SmearedElectronsProducer.HF.constantm = 0.152
+#c-HFp-down
+#process.SmearedElectronsProducer.HF.constantp = 0.16
+#c-HFp-up
+#process.SmearedElectronsProducer.HF.constantp = 0.188
 
 
 process.p = cms.Path(process.SmearedElectronsProducer
                      + process.mcEff
                      )
+
