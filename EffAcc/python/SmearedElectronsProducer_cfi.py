8eacd2b5950345cb95607c6096d0a5cd27fc8f05
Jason Haupt <haupt@physics.umn.edu>
2011-01-19 07:17:42 +0000

    newest smearing

--- EffAcc/python/SmearedElectronsProducer_cfi.py
+++ EffAcc/python/SmearedElectronsProducer_cfi.py
@@ -11,27 +11,29 @@ SmearedElectronsProducer = cms.EDProducer("ZSmearingProducer",
     randomSeed = cms.int32(134),
     SmearTable = cms.FileInPath('ZShape/EffAcc/data/CBBinsFile.txt'),
     HF = cms.PSet(
-       stocastic = cms.double(3.45626),
-       constant = cms.double(0.08651),
-       mean = cms.double(1.04346)
-        #tuned by kevin on 04-16-09, mean added by J. Mans 05-22-09
+       stocastic = cms.double(2.08),
+       constantp = cms.double(0.1739),
+       constantm = cms.double(0.1398),
+       meanp = cms.double(1.0404),
+       meanm = cms.double(1.0377)
+       #tuned by kevin on 04-16-09, mean added by J. Mans 05-22-09
     ),
     EB = cms.PSet(
        #tuned by j. Mans on 06-08-09
-       p0 = cms.double(0.0416507) , 
+       p0 = cms.double(0.04072) , 
        p1 = cms.double(1.06),
        p2 = cms.double(4.78),
        c = cms.double(0.009), #0.004
-       alpha = cms.double(1.379),
-       mean = cms.double(0.999956), #back to 1.0 from 0.983
+       alpha = cms.double(1.40116),
+       mean = cms.double(1.00016), #back to 1.0 from 0.983
        n = cms.double(2.0) 	   
     ),
      EE = cms.PSet(
-       p0 = cms.double(1.62647),
+       p0 = cms.double(1.52331),
        p1 = cms.double(0.72),
        p2 = cms.double(0.15), #0.15
-       alpha = cms.double(1.61528),
-       mean = cms.double(0.977413),
+       alpha = cms.double(1.67726),
+       mean = cms.double(0.97774),
        n = cms.double(2.0) 	
     )
                                           
