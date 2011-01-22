dd7429420587e5253d80dede2dd789410f240210
franzoni <>
2011-01-22 10:59:52 +0000

    - CHANGE in STRATEGY: use migration matrix in "All" folder   (before was a combination of "ECAL80-ECAL95"+"ECAL80-HF")   to be consistent with the overall strategy of the paper

--- Paper/root/readUnfoldingMatrices.C
+++ Paper/root/readUnfoldingMatrices.C
@@ -16,6 +16,7 @@
 //      root [1] readUnfoldingMatrices("unfoldingMatrix_theOutPut.root","effAccSource.root")
 
 
+
 TH1* unfold(const TH1* source, const char* unfoldingMatrixIF)  {
   // load the matrix...
 
@@ -23,10 +24,16 @@ TH1* unfold(const TH1* source, const char* unfoldingMatrixIF)  {
 
   TMatrixD * theUnfoldingMatrix = (TMatrixD*)theunfoldingMatrixInputFile.Get("unsmearMatrices/unfoldingMatrixTotal");
 
+  unfold(source, theUnfoldingMatrix);
+}
+
+
+TH1* unfold(const TH1* source, TMatrixD * theUnfoldingMatrix)  {
   Double_t arrayRapiditySmeared[100]; // this needs be hardcoded...
   for(int bin=1; bin<=100; bin++)
     {
       arrayRapiditySmeared[bin-1]=source->GetBinContent(bin);
+      if (0) std::cout << source->GetBinContent(bin) << std::endl;
     }
 
   TVectorD vectorRapiditySmeared; vectorRapiditySmeared.Use(100,arrayRapiditySmeared);
@@ -45,10 +52,13 @@ TH1* unfold(const TH1* source, const char* unfoldingMatrixIF)  {
   for(int bin=1; bin<=100; bin++)
   {
    h_RapidityUNSmeared->SetBinContent(bin,vectorRapidityUNSmeared[bin-1]);
-   h_RapidityUNSmeared->SetBinError(bin,vectorRapidityUNSmeared[bin-1]/vectorRapiditySmeared[bin-1]*source->GetBinError(bin));
+   if(fabs(vectorRapiditySmeared[bin-1]) > 1.e-9) h_RapidityUNSmeared->SetBinError(bin,vectorRapidityUNSmeared[bin-1]/vectorRapiditySmeared[bin-1]*source->GetBinError(bin));
+   else                                           h_RapidityUNSmeared->SetBinError(bin,source->GetBinError(bin));
   }
   return h_RapidityUNSmeared;
 }
+
+
 
 
 int readUnfoldingMatrices(std::string unfoldingMatrixFileInputFile, std::string effAccFileForTest) 
@@ -69,8 +79,9 @@ int readUnfoldingMatrices(std::string unfoldingMatrixFileInputFile, std::string 
     
     // this is the matrix you want to use for to unfold
     TMatrixD * theUnfoldingMatrix = (TMatrixD*)theunfoldingMatrixInputFile->Get("unsmearMatrices/unfoldingMatrixTotal");
-    
 
+    /*
+    //////////GFfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
     // now pull up a test ECAL-ECAL histogram 
     std::string prefix("mcEff/");
     std::string definition("ECAL80-ECAL95");
@@ -82,7 +93,11 @@ int readUnfoldingMatrices(std::string unfoldingMatrixFileInputFile, std::string 
     std::cout << "\n\n\tUnsing ECAL-ECAL plot : "<< plotName << endl;
     TH1F* h_smearedRapidityECALECAL = (TH1F*) theEffAccInpuntFile->Get(plotName.c_str());
     std::cout << "\tname h_smearedRapidityECALECAL: " << h_smearedRapidityECALECAL->GetName() << std::endl;
+    plot=std::string("Z0_YTL");                  // tree level histogram
+    plotName = prefix + definition + std::string("/") +  cut + std::string("/") + plot;
+    TH1F* h_treeLevelRapidityECALECAL = (TH1F*) theEffAccInpuntFile->Get(plotName.c_str());
 
+    std::cout <<  plotName << "    " << h_treeLevelRapidityECALECAL << std::endl;
 
     // now pull up a test ECAL-HF histogram 
     definition=std::string("ECAL80-HF");
@@ -93,6 +108,9 @@ int readUnfoldingMatrices(std::string unfoldingMatrixFileInputFile, std::string 
     std::cout << "\tUsing ECAL-HF plot:     "<< plotName << endl;
     TH1F* h_smearedRapidityECALHF = (TH1F*) theEffAccInpuntFile->Get(plotName.c_str());
     std::cout << "\tname h_smearedRapidityECALHF: " <<  h_smearedRapidityECALHF->GetName() <<  std::endl;
+    plot=std::string("Z0_YTL");                  // tree level histogram
+    plotName = prefix + definition + std::string("/") +  cut + std::string("/") + plot;
+    TH1F* h_treeLevelRapidityECALHF = (TH1F*) theEffAccInpuntFile->Get(plotName.c_str());
 
    // building total migration histogram, summing the  ECAL-ECAL and the ECAL-HF histograms
    TH1F * h_fullTest_smeared  = new TH1F("h_fullTest_smeared","h_fullTest_smeared", 
@@ -102,6 +120,41 @@ int readUnfoldingMatrices(std::string unfoldingMatrixFileInputFile, std::string 
 	
    h_fullTest_smeared->Add(h_smearedRapidityECALHF,h_smearedRapidityECALECAL);
    h_fullTest_smeared->GetXaxis()->SetTitle("Y_{RECO} for test");
+
+
+   // building total tree level histogram, summing the  ECAL-ECAL and the ECAL-HF histograms
+   TH1F * h_fullTest_treeLevel  = new TH1F("h_fullTest_TL","h_fullTest_TL", 
+		h_smearedRapidityECALHF->GetNbinsX(),
+		h_smearedRapidityECALHF->GetXaxis()->GetXmin(),
+		h_smearedRapidityECALHF->GetXaxis()->GetXmax());
+   
+   h_fullTest_treeLevel->Add(h_treeLevelRapidityECALHF,h_treeLevelRapidityECALECAL);
+   h_fullTest_treeLevel->GetXaxis()->SetTitle("Y_{RECO} for test");
+
+   //////////GFfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+    */
+
+    //////////////////////////////////////////////////////
+    // ... but to be consistent with the definitions 
+    // of AN-10-367 one needs to use the YZTL_vs_YZ before Eff and Acc
+    // are applied ==> move to the "All" folder
+    // get hold of ECAL-HF 2d plot 
+    std::string prefix("mcEff/");
+    std::string definition=std::string("All");
+    std::string cut("");
+    std::string plot=std::string("Z0_Y"); // smeared Y before Eff&Acc
+    std::string plotName = prefix + definition;
+    plotName = plotName + std::string("/");    plotName = plotName + cut;
+    plotName = plotName + std::string("/");    plotName = plotName + plot;
+    std::cout << "\tUsing directly the total smeared Y plot:     "<< plotName << endl;
+    TH2F * h_fullTest_smeared = (TH2F*) theEffAccInpuntFile->Get(plotName.c_str());
+
+    plot=std::string("Z0_YTL"); // tree-level Y before Eff&Acc
+    plotName = prefix + definition;
+    plotName = plotName + std::string("/");    plotName = plotName + cut;
+    plotName = plotName + std::string("/");    plotName = plotName + plot;
+    std::cout << "\tComparing agains the total tree-Level Y plot:     "<< plotName << endl;
+    TH2F * h_fullTest_treeLevel = (TH2F*) theEffAccInpuntFile->Get(plotName.c_str());
 
 
 
@@ -119,9 +172,9 @@ int readUnfoldingMatrices(std::string unfoldingMatrixFileInputFile, std::string 
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   TH1D * h_RapidityUNSmeared = new TH1D("Rapidity Unsmeared", "Rapidity Unsmeared", 
-		h_smearedRapidityECALHF->GetNbinsX(),
-		h_smearedRapidityECALHF->GetXaxis()->GetXmin(),
-		h_smearedRapidityECALHF->GetXaxis()->GetXmax());
+		h_fullTest_treeLevel->GetNbinsX(),
+		h_fullTest_treeLevel->GetXaxis()->GetXmin(),
+		h_fullTest_treeLevel->GetXaxis()->GetXmax());
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   // turning the unsmeared vector into a histogram
   for(int bin=1; bin<=100; bin++)
@@ -130,11 +183,28 @@ int readUnfoldingMatrices(std::string unfoldingMatrixFileInputFile, std::string 
    //std::cout << vectorRapidityUNSmeared[bin-1]<<std::endl;
   }
 
+  gStyle->SetOptStat(0);
+  TCanvas * theBaseCanvas = new TCanvas("check of closure","check of closure",200,200,1000,800);  
+  theBaseCanvas->cd();
   h_RapidityUNSmeared->Draw();
-  h_fullTest_smeared->SetLineColor(kRed);
+
+  h_fullTest_smeared->SetLineColor(kGreen);
   h_fullTest_smeared->Draw("same");
 
-  std::cout << "\n\n\n\t If you see two plots overlayed, you've applied unfolding matrix succesfully" << std::endl;
+  h_fullTest_treeLevel->SetLineColor(kRed);
+  h_fullTest_treeLevel->SetLineStyle(8);
+  h_fullTest_treeLevel->Draw("same");
+  
+  TLegend* leg = new TLegend(0.7,0.7,0.95,0.9);
+  leg->SetHeader("closure: isn't it?");
+  leg->AddEntry(h_fullTest_treeLevel,"tree level","lp");
+  leg->AddEntry(h_RapidityUNSmeared,"unsmeared: does it overlap?","lp");
+  leg->AddEntry(h_fullTest_smeared,"smeared","lp");
+  leg->SetFillColor(0); leg->SetShadowColor(0); leg->SetTextSize(0.02);
+  leg->Draw();  
+  
+
+  std::cout << "\n\n\n\t If you see tree plots and two are overlayed (gree and black), you've applied unfolding matrix succesfully" << std::endl;
 
   return 0;
 }
