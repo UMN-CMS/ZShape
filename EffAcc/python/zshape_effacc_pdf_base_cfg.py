d47077154afab1b6895ee228e15c95a71914695c
Jeremiah Mans <jmmans@physics.umn.edu>
2011-11-15 20:28:10 +0000

    PDF configurations

---
+++ EffAcc/python/zshape_effacc_pdf_base_cfg.py
@@ -0,0 +1,29 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Zefficiency")
+process.TimerService = cms.Service("TimerService")
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True),
+    SkipEvent = cms.untracked.vstring('ProductNotFound') 
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+    )
+
+process.source = cms.Source("PoolSource",
+                            duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
+    fileNames = cms.untracked.vstring("file:///local/cms/user/pastika/powhegZ/base_PDF-10901/PythiaUEZ2/Zee_Powheg_PythiaUEZ2_1.root" )
+)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('histo_10M_partBUILDINGTTEST.root')
+)
+
+process.load("ZShape.EffAcc.SmearedElectronsProducer_cfi")
+import ZShape.EffAcc.ZEfficiencyPDF_cfi 
+
+
