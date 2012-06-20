5fd34cdd222d007a8ab47e9bf478f66c5c53e6f7
mansj <>
2012-06-20 20:04:53 +0000

    Updates from 5X

--- HFZeeVBTF/test/skimHFCalib_cfg.py
+++ HFZeeVBTF/test/skimHFCalib_cfg.py
@@ -1,7 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("PAT")
-
+process = cms.Process("CALSKIM")
 
 process.load('FWCore/MessageService/MessageLogger_cfi')
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
@@ -24,7 +23,7 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 ## global tags:
-process.GlobalTag.globaltag = cms.string('GR_P_V16::All')
+process.GlobalTag.globaltag = cms.string('GR_R_50_V13::All')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load('Configuration.StandardSequences.Services_cff')
 
@@ -110,114 +109,6 @@ process.makePatCandidates = cms.Sequence(process.makePatElectrons+process.makePa
 process.patDefaultSequence = cms.Sequence(process.makePatCandidates)
 
 
-##
-##  ################################################################################
-##
-##  the filter to select the candidates from the data samples
-##
-##
-## WARNING: you may want to modify this item:
-HLT_process_name = "HLT"   # 
-# trigger path selection
-# HLT_path_name    = "HLT_Ele15_LW_L1R"
-HLT_path_name     = "HLT_Photon10_L1R"
-# trigger filter name
-HLT_filter_name  =  "hltL1NonIsoHLTNonIsoSinglePhotonEt10HcalIsolFilter"
-# HLT_filter_name  =  "hltL1NonIsoHLTNonIsoSingleElectronLWEt15PixelMatchFilter"
-#
-HLT_path_name_extra   = "HLT_Photon15_L1R" #= "HLT_Ele15_LW_L1R" #
-HLT_filter_name_extra = "hltL1NonIsoHLTNonIsoSinglePhotonEt15HcalIsolFilter"
-process.z1legFilter=cms.EDFilter('WenuCandidateFilter',
-                                  ### the input collections needed:
-                                  electronCollectionTag = cms.untracked.InputTag("patElectrons","","PAT"),
-                                  metCollectionTag = cms.untracked.InputTag(myMetCollection,"","PAT"),
-                                  pfMetCollectionTag = cms.untracked.InputTag(myPfMetCollection,"","PAT"),
-                                  tcMetCollectionTag = cms.untracked.InputTag(myTcMetCollection,"","PAT"),
-                                  triggerCollectionTag = cms.untracked.InputTag("TriggerResults","",HLT_process_name),
-                                  triggerEventTag = cms.untracked.InputTag("hltTriggerSummaryAOD","",HLT_process_name),
-                                  hltpath = cms.untracked.string(HLT_path_name),
-                                  hltpathFilter = cms.untracked.InputTag(HLT_filter_name,"",HLT_process_name),
-                                  ebRecHits = cms.untracked.InputTag("reducedEcalRecHitsEB"),
-                                  eeRecHits = cms.untracked.InputTag("reducedEcalRecHitsEE"),
-                                  PrimaryVerticesCollection = cms.untracked.InputTag("offlinePrimaryVertices"),
-                                  ### here the preselection is applied
-                                  # fiducial cuts:
-                                  BarrelMaxEta = cms.untracked.double(1.4442),
-                                  EndCapMinEta = cms.untracked.double(1.566),
-                                  EndCapMaxEta = cms.untracked.double(2.5),
-                                  # demand ecal driven electron:
-                                  useEcalDrivenElectrons = cms.untracked.bool(True),
-                                  # demand offline spike cleaning with the Swiss Cross criterion:
-                                  useSpikeRejection = cms.untracked.bool(True),
-                                  spikeCleaningSwissCrossCut = cms.untracked.double(0.95),
-                                  # demand geometrically matched to an HLT object with ET>15GeV
-                                  useTriggerInfo = cms.untracked.bool(False), # GF 
-                                  electronMatched2HLT = cms.untracked.bool(False), # GF 
-                                  electronMatched2HLT_DR = cms.untracked.double(0.2), # GF 
-                                  useHLTObjectETCut = cms.untracked.bool(False), # GF 
-                                  hltObjectETCut = cms.untracked.double(15.),
-                                  useExtraTrigger = cms.untracked.bool(False), # GF 
-                                  hltpathExtra = cms.untracked.string(HLT_path_name_extra),
-                                  hltpathFilterExtra = cms.untracked.InputTag(HLT_filter_name_extra,"",HLT_process_name),
-                                  # ET Cut in the SC
-                                  ETCut = cms.untracked.double(15.), 
-                                  METCut = cms.untracked.double(0.),
-                                  # reject events with a 2nd electron with ET > 20 that passes the WP95%
-                                  vetoSecondElectronEvents = cms.untracked.bool(False),
-                                  storeSecondElectron = cms.untracked.bool(False),
-                                  ETCut2ndEle = cms.untracked.double(20.),
-                                  vetoSecondElectronIDType = cms.untracked.string("simpleEleId80relIso"), # GF
-                                  vetoSecondElectronIDSign = cms.untracked.string("="),
-                                  vetoSecondElectronIDValue = cms.untracked.double(7.),
-                                  # Other parameters of the code - leave them as they are
-                                  useValidFirstPXBHit = cms.untracked.bool(False),
-                                  useConversionRejection = cms.untracked.bool(False),
-                                  useExpectedMissingHits = cms.untracked.bool(False),
-                                  maxNumberOfExpectedMissingHits = cms.untracked.int32(1),
-                                  # calculate some new cuts
-                                  calculateValidFirstPXBHit = cms.untracked.bool(True),
-                                  calculateConversionRejection = cms.untracked.bool(True),
-                                  calculateExpectedMissingHits = cms.untracked.bool(True),
-                                  # we are dealing with DATA
-                                  dataMagneticFieldSetUp = cms.untracked.bool(False),
-                                  dcsTag = cms.untracked.InputTag("scalersRawToDigi"),
-                                  )
-
-# = cms.EDFilter('WenuCandidateFilter',
-#                                   # cfg for data
-#                                   dataMagneticFieldSetUp = cms.untracked.bool(False), # set to False in MC!
-#                                   dcsTag = cms.untracked.InputTag("scalersRawToDigi"),
-#                                   # cuts
-#                                   ETCut = cms.untracked.double(10.),
-#                                   METCut = cms.untracked.double(0.),
-#                                   # 2nd electron in W events
-#                                   vetoSecondElectronEvents = cms.untracked.bool(False),
-#                                   ETCut2ndEle = cms.untracked.double(20.),
-#                                   vetoSecondElectronIDType = cms.untracked.string("simpleEleId80relIso"),
-#                                   vetoSecondElectronIDSign = cms.untracked.string("="),
-#                                   vetoSecondElectronIDValue = cms.untracked.double(7.),
-#                                   # trigger 
-#                                   useTriggerInfo = cms.untracked.bool(False),
-#                                   triggerCollectionTag = cms.untracked.InputTag("TriggerResults","",HLT_process_name),
-#                                   triggerEventTag = cms.untracked.InputTag("hltTriggerSummaryAOD","",HLT_process_name),
-#                                   hltpath = cms.untracked.string(HLT_path_name), 
-#                                   hltpathFilter = cms.untracked.InputTag(HLT_filter_name,"",HLT_process_name),
-#                                   electronMatched2HLT = cms.untracked.bool(False),
-#                                   electronMatched2HLT_DR = cms.untracked.double(0.2),
-#                                   # additional preselection cuts
-#                                   useValidFirstPXBHit = cms.untracked.bool(False),
-#                                   useConversionRejection = cms.untracked.bool(False),
-#                                   useExpectedMissingHits = cms.untracked.bool(False),
-#                                   maxNumberOfExpectedMissingHits = cms.untracked.int32(1),
-#                                   # calculate some new cuts
-#                                   calculateValidFirstPXBHit = cms.untracked.bool(True),
-#                                   calculateConversionRejection = cms.untracked.bool(True),
-#                                   calculateExpectedMissingHits = cms.untracked.bool(True),
-#                                   # electrons and MET
-#                                   electronCollectionTag = cms.untracked.InputTag("patElectrons","","PAT"),
-#                                   metCollectionTag = cms.untracked.InputTag(myDesiredMetCollection,"","PAT"),
-# 
-#                                   )
 ####################################################################################
 
 # to access values of EldId cuts as defined by the EWK group
@@ -261,9 +152,8 @@ process.outpath = cms.EndPath(process.zCOutputModule)
 
 process.zCalibPath = cms.Path(
     process.patDefaultSequence *
-    process.hfzeeBefCuts *
-    process.z1legFilter *
     process.hfRecoEcalCandidate *
+    process.hfzeeBefCuts *
     process.hfzeeForCalib 
     )
 
