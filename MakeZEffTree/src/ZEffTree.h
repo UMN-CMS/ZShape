d7c5a781ac7f69e0d1dafe75f98e7f9954f4c9ab
Alexander Gude <gude@physics.umn.edu>
2012-09-14 19:10:48 +0000

    Adding phistar storage.

--- MakeZEffTree/src/ZEffTree.h
+++ MakeZEffTree/src/ZEffTree.h
@@ -24,6 +24,7 @@ class ZEffTree {
             bool isSelected(int ielec, const std::string& bitname) const;
             void setBit(int ielec, const std::string& bitname, bool val);
             int charge[2];
+            float phistar;
         } gen, reco;
 
         void Fill() { 
@@ -48,8 +49,8 @@ class ZEffTree {
         }
 
         void Clear() { 
-            gen.eta[0] = 0; gen.eta[1] = 0; gen.phi[0] = 0; gen.phi[1] = 0; gen.pt[0] = 0; gen.pt[1] = 0; gen.mz = 0; gen.yz = 0; gen.qtz = 0; gen.bits[0] = 0; gen.bits[1] = 0; gen.nverts = 0; gen.charge[0] = 0; gen.charge[1] = 0;
-            reco.eta[0] = 0; reco.eta[1] = 0; reco.phi[0] = 0; reco.phi[1] = 0; reco.pt[0] = 0; reco.pt[1] = 0; reco.mz = 0; reco.yz = 0; reco.qtz = 0; reco.bits[0] = 0; reco.bits[1] = 0; reco.nverts = 0; reco.charge[0] = 0; reco.charge[1] = 0;
+            gen.eta[0] = 0; gen.eta[1] = 0; gen.phi[0] = 0; gen.phi[1] = 0; gen.pt[0] = 0; gen.pt[1] = 0; gen.mz = 0; gen.yz = 0; gen.qtz = 0; gen.bits[0] = 0; gen.bits[1] = 0; gen.nverts = 0; gen.charge[0] = 0; gen.charge[1] = 0; gen.phistar = -999.;
+            reco.eta[0] = 0; reco.eta[1] = 0; reco.phi[0] = 0; reco.phi[1] = 0; reco.pt[0] = 0; reco.pt[1] = 0; reco.mz = 0; reco.yz = 0; reco.qtz = 0; reco.bits[0] = 0; reco.bits[1] = 0; reco.nverts = 0; reco.charge[0] = 0; reco.charge[1] = 0; reco.phistar = -999.;
         }
 
     private:
@@ -63,8 +64,8 @@ class ZEffTree {
             if (writable) {
                 m_file.cd();
                 m_tree=new TTree("ZEffs","Minnesota ZEffs");
-                br_gen=m_tree->Branch("gen",&gen,"eta0/f:eta1:phi0:phi1:pt0:pt1:mz:yz:qtz:bits0/I:bits1:nverts:charge0:charge1");
-                br_reco=m_tree->Branch("reco",&reco,"eta0/f:eta1:phi0:phi1:pt0:pt1:mz:yz:qtz:bits0/I:bits1:nverts:charge0:charge1");
+                br_gen=m_tree->Branch("gen",&gen,"eta0/f:eta1:phi0:phi1:pt0:pt1:mz:yz:qtz:bits0/I:bits1:nverts:charge0:charge1:phistar/f");
+                br_reco=m_tree->Branch("reco",&reco,"eta0/f:eta1:phi0:phi1:pt0:pt1:mz:yz:qtz:bits0/I:bits1:nverts:charge0:charge1:phistar/f");
             } else {
                 m_tree=(TTree*)m_file.Get("ZEffs");
                 m_tree->SetBranchAddress("gen",&gen);
