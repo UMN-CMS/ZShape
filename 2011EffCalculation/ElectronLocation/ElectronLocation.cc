61b7e9fa70045c8479f31bb55af05a599e4b0274
Alexander Gude <gude@physics.umn.edu>
2013-07-16 10:10:46 -0500

    Fixed bug in ElectronLocation.cc
    The function inAcceptance didn't have breaks in the case statement. I
    incorrectly concluded I didn't need them because we had if statements,
    but not including them caused some cases to drop through until they were
    true, and then return true incorrectly.
    
    I've also expanded the lines, there is no reason to try to keep the
    function compact in lines.

--- 2011EffCalculation/ElectronLocation/ElectronLocation.cc
+++ 2011EffCalculation/ElectronLocation/ElectronLocation.cc
@@ -4,31 +4,70 @@ bool inAcceptance(const electronLocation Loc, const double eta){
     const double feta = fabs(eta);
     switch (Loc){
         case EB:
-            if (feta < 1.4442){ return true; }
+            if (feta < 1.4442){
+                return true; 
+            }
+            break;
         case EBp:
-            if (0. < eta && eta < 1.4442){ return true; }
+            if (0. < eta && eta < 1.4442){
+                return true; 
+            }
+            break;
         case EBm:
-            if (0. > eta && eta > -1.4442){ return true; }
+            if (0. > eta && eta > -1.4442){
+                return true; 
+            }
+            break;
         case EE:
-            if (1.566 < feta && feta < 2.5){ return true; }
+            if (1.566 < feta && feta < 2.5){
+                return true; 
+            }
+            break;
         case EEp:
-            if (1.566 < eta && eta < 2.5){ return true; }
+            if (1.566 < eta && eta < 2.5){
+                return true; 
+            }
+            break;
         case EEm:
-            if (-1.566 > eta && eta > -2.5){ return true; }
+            if (-1.566 > eta && eta > -2.5){
+                return true; 
+            }
+            break;
         case ET:
-            if (feta < 1.4442 || (1.566 < feta && feta < 2.5)){ return true; }
+            if (feta < 1.4442 || (1.566 < feta && feta < 2.5)){
+                return true; 
+            }
+            break;
         case NT:
-            if (2.5 < feta && feta < 2.850){ return true; }
+            if (2.5 < feta && feta < 2.850){
+                return true; 
+            }
+            break;
         case NTp:
-            if (2.5 < eta && eta < 2.850){ return true; }
+            if (2.5 < eta && eta < 2.850){
+                return true; 
+            }
+            break;
         case NTm:
-            if (-2.5 > eta && eta > -2.850){ return true; }
+            if (-2.5 > eta && eta > -2.850){
+                return true; 
+            }
+            break;
         case HF:
-            if (3.1 < feta && feta < 4.6){ return true; }
+            if (3.1 < feta && feta < 4.6){
+                return true; 
+            }
+            break;
         case HFp:
-            if (3.1 < eta && eta < 4.6){ return true; }
+            if (3.1 < eta && eta < 4.6){
+                return true; 
+            }
+            break;
         case HFm:
-            if (-3.1 > eta && eta > -4.6){ return true; }
+            if (-3.1 > eta && eta > -4.6){
+                return true; 
+            }
+            break;
     }
     return false;
 }
