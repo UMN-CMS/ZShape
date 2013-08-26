de093cbff9dbfd03d902cf085140a27189cd3224
Alexander Gude <gude@physics.umn.edu>
2013-08-26 16:00:11 -0500

    Update PlotZPeak
    Used this version to send a plot to Jeremy. Now with labels.

--- MakeZEffTree/misc/PlotZPeak.cc
+++ MakeZEffTree/misc/PlotZPeak.cc
@@ -6,6 +6,7 @@
 #include "TH1.h"
 #include "TFile.h"
 #include "TCanvas.h"
+#include "TLatex.h"  // Used to draw text on the plot
 #include <TROOT.h>  // Pulls in gROOT
 #include <TStyle.h>  // Pulls in gStyle
 
@@ -18,14 +19,33 @@ int makeTupleCuts(const std::string inputFile, const std::string outFile){
     gROOT->SetStyle("Plain");
     gStyle->SetOptTitle(0);  // Remove title
     gStyle->SetOptStat(0);  // Remove Stats Box
+    gStyle->SetHistLineWidth(2);  // Remove Stats Box
 
+    /* Canvas */
     TCanvas* canvas = new TCanvas("ZPeak", "ZPeak", 1200, 900);
+    //canvas->SetLeftMargin(0.25);
+    canvas->SetRightMargin(0.03);
+    canvas->SetTopMargin(0.02);
+
+    /* Text */
+    // CMS Preliminary
+    //TLatex* cmsPrelim = new TLatex(0.12, 0.97, "CMS Preliminary");
+    //cmsPrelim->SetTextAlign(13);  // Align by top left of the words
+    TLatex* cmsPrelim = new TLatex(0.97, 0.97, "CMS Preliminary");
+    cmsPrelim->SetTextAlign(33);  // Align by top left of the words
+    cmsPrelim->SetNDC();  // Draw according to pad coordinates, not unitful coordinates
+    // Plot Title
+    TLatex* plotTitle = new TLatex(0.97, 0.97, "ECAL-HF Invarient Mass");
+    plotTitle->SetTextAlign(33);  // Align by top left of the words
+    plotTitle->SetNDC();  // Draw according to pad coordinates, not unitful coordinates
 
     /* Prepare the histogram to put the data in */
     const std::string Z0MassName = "Z0_mass";
     TH1I* Z0Mass = new TH1I(Z0MassName.c_str(), Z0MassName.c_str(), 100, 50., 150.);
     Z0Mass->GetXaxis()->SetTitle("m_{ee} [GeV]");
     Z0Mass->GetYaxis()->SetTitle("Counts/GeV");
+    Z0Mass->GetYaxis()->SetTitleOffset(1.3);  // Move axis title away from side to avoid clipping
+    Z0Mass->GetYaxis()->SetTitleSize(0.04);  // Make Axis title larger
 
     // Open file to fit, and make histograms
     TFile ZEffFile(inputFile.c_str(), "READ");
@@ -61,6 +81,8 @@ int makeTupleCuts(const std::string inputFile, const std::string outFile){
     }
 
     Z0Mass->Draw();
+    cmsPrelim->Draw();
+    //plotTitle->Draw();
     canvas->Print(outFile.c_str());
 
     return 0;
