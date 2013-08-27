e2fd69bce85ea58c37004b7af8228168fdf03b47
Alexander Gude <gude@physics.umn.edu>
2013-08-27 16:28:03 -0500

    Update PlotZPeak to use MC

--- MakeZEffTree/misc/PlotZPeak.cc
+++ MakeZEffTree/misc/PlotZPeak.cc
@@ -14,7 +14,7 @@
 #include <string>
 #include <sstream>
 
-int makeTupleCuts(const std::string inputFile, const std::string outFile){
+int makeTupleCuts(const std::string inputFile, const std::string inputMCFile, const std::string outFile){
     // Root style
     gROOT->SetStyle("Plain");
     gStyle->SetOptTitle(0);  // Remove title
@@ -29,21 +29,20 @@ int makeTupleCuts(const std::string inputFile, const std::string outFile){
 
     /* Text */
     // CMS Preliminary
-    //TLatex* cmsPrelim = new TLatex(0.12, 0.97, "CMS Preliminary");
-    //cmsPrelim->SetTextAlign(13);  // Align by top left of the words
     TLatex* cmsPrelim = new TLatex(0.95, 0.95, "CMS Preliminary");
     cmsPrelim->SetTextAlign(33);  // Align by top left of the words
     cmsPrelim->SetNDC();  // Draw according to pad coordinates, not unitful coordinates
     // Luminosity
     TLatex* luminosity = new TLatex(0.95, 0.90, "5.5 fb^{-1} at 7 TeV");
-    luminosity->SetTextAlign(33);  // Align by top left of the words
-    luminosity->SetNDC();  // Draw according to pad coordinates, not unitful coordinates
+    luminosity->SetTextAlign(33);
+    luminosity->SetNDC();
     // Plot Title
     //TLatex* plotTitle = new TLatex(0.97, 0.97, "ECAL-HF Invarient Mass");
     //plotTitle->SetTextAlign(33);  // Align by top left of the words
     //plotTitle->SetNDC();  // Draw according to pad coordinates, not unitful coordinates
 
     /* Prepare the histogram to put the data in */
+    // Data
     const std::string Z0MassName = "Z0_mass";
     TH1I* Z0Mass = new TH1I(Z0MassName.c_str(), Z0MassName.c_str(), 100, 50., 150.);
     Z0Mass->GetXaxis()->SetTitle("m_{ee} [GeV]");
@@ -52,14 +51,25 @@ int makeTupleCuts(const std::string inputFile, const std::string outFile){
     Z0Mass->GetYaxis()->SetTitleSize(0.04);  // Make Axis title larger
     Z0Mass->SetMarkerStyle(20);  // Use black circles for points
     Z0Mass->SetMarkerSize(0.5);  // Make markers half the normal size
+    // MC
+    const std::string Z0MassMCName = "Z0_mass_MC";
+    TH1I* Z0MassMC = new TH1I(Z0MassMCName.c_str(), Z0MassMCName.c_str(), 100, 50., 150.);
+    Z0MassMC->GetXaxis()->SetTitle("m_{ee} [GeV]");
+    Z0MassMC->GetYaxis()->SetTitle("Counts/GeV");
+    Z0MassMC->GetYaxis()->SetTitleOffset(1.3);  // Move axis title away from side to avoid clipping
+    Z0MassMC->GetYaxis()->SetTitleSize(0.04);  // Make Axis title larger
+    Z0MassMC->SetMarkerStyle(20);  // Use black circles for points
+    Z0MassMC->SetMarkerSize(0.5);  // Make markers half the normal size
+    Z0MassMC->SetMarkerColor(2);  // Make markers half the normal size
 
     // Open file to fit, and make histograms
+    // Data
     TFile ZEffFile(inputFile.c_str(), "READ");
     ZEffTree* ze;
     ze = new ZEffTree(ZEffFile, false);
 
-    bool run = true;
-    while (run){
+    bool runMC = true;
+    while (runMC){
         ze->Entries();
         int ETElectron;
         int HFElectron;
@@ -73,21 +83,61 @@ int makeTupleCuts(const std::string inputFile, const std::string outFile){
             ETElectron = 1;
             HFElectron = 0;
         } else {
-            run = ze->GetNextEvent();
+            runMC = ze->GetNextEvent();
             continue;
         }
 
         /* Cuts */
         if ( ze->reco.pt[0] > 20. && ze->reco.pt[1] > 20.){
             if ( ze->reco.isSelected(ETElectron, "WP80") && ze->reco.isSelected(HFElectron, "HFTightElectronId-EtaDet" )){
-            //if ( ze->reco.isSelected(ETElectron, "WP80")){
                 Z0Mass->Fill(MZ);
             }
         }
-        run = ze->GetNextEvent();
+        runMC = ze->GetNextEvent();
     }
 
-    Z0Mass->Draw("E");
+    // MC
+    TFile ZEffMCFile(inputMCFile.c_str(), "READ");
+    ZEffTree* zemc;
+    zemc = new ZEffTree(ZEffMCFile, false);
+
+    bool run = true;
+    while (run){
+        zemc->Entries();
+        int ETElectron;
+        int HFElectron;
+        const double MZ = zemc->reco.mz;
+        const short PU = zemc->reco.nverts;
+        /* Assign Electron 0 and 1 */
+        if ( inAcceptance(ET, zemc->reco.eta[0]) && inAcceptance(HF, zemc->reco.eta[1])){
+            ETElectron = 0;
+            HFElectron = 1;
+        } else if ( inAcceptance(HF, zemc->reco.eta[1]) && inAcceptance(ET, zemc->reco.eta[0])){
+            ETElectron = 1;
+            HFElectron = 0;
+        } else {
+            run = zemc->GetNextEvent();
+            continue;
+        }
+
+        /* Cuts */
+        if ( zemc->reco.pt[0] > 20. && zemc->reco.pt[1] > 20.){
+            if ( zemc->reco.isSelected(ETElectron, "WP80") && zemc->reco.isSelected(HFElectron, "HFTightElectronId-EtaDet" )){
+                Z0MassMC->Fill(MZ);
+            }
+        }
+        run = zemc->GetNextEvent();
+    }
+
+    // Scale histograms and preserve erros
+    Z0Mass->Sumw2();
+    Z0MassMC->Sumw2();
+    const double areaData = Z0Mass->Integral(Z0Mass->FindBin(50), Z0Mass->FindBin(150));
+    const double areaMC = Z0MassMC->Integral(Z0MassMC->FindBin(50), Z0MassMC->FindBin(150));
+    Z0MassMC->Scale(areaData/areaMC);
+
+    Z0MassMC->Draw("E");
+    Z0Mass->Draw("E Same");
     cmsPrelim->Draw();
     luminosity->Draw();
     //plotTitle->Draw();
@@ -98,12 +148,12 @@ int makeTupleCuts(const std::string inputFile, const std::string outFile){
 }
 
 int main(int argc, char* argv[]){
-    const short argcCorrect = 3;
+    const short argcCorrect = 4;
     if (argc < argcCorrect) {
-        std::cout<<"Not enough arguments. Use:\nPlotZPeak.exe ZEffFile outputFile\n";
+        std::cout<<"Not enough arguments. Use:\nPlotZPeak.exe ZEffFile_data ZEffFile_MC outputFile\n";
         return 1;
     } else if (argc > argcCorrect){
-        std::cout<<"Too many arguments. Use:\nPlotZPeak.exe ZEffFile outputFile\n";
+        std::cout<<"Too many arguments. Use:\nPlotZPeak.exe ZEffFile_data ZEffFile_MC outputFile\n";
         return 1;
     } else {
         // Read in arguments
@@ -114,11 +164,16 @@ int main(int argc, char* argv[]){
         inStream >> inputFile;
         inStream.clear();
 
-        std::string outFile;
+        std::string inputMCFile;
         inStream.str(argv[2]);
+        inStream >> inputMCFile;
+        inStream.clear();
+
+        std::string outFile;
+        inStream.str(argv[3]);
         inStream >> outFile;
         inStream.clear();
 
-        return makeTupleCuts(inputFile, outFile);
+        return makeTupleCuts(inputFile, inputMCFile, outFile);
     }
 }
