11813ac6db118d1dc97e8f31b41b1e5d1c877f6d
Kevin Klapoetke <klapoetke@physics.umn.edu>
2013-09-17 10:43:43 -0500

    Remove autogenerated __init__.py

--- ZFromData/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/ZShape/ZFromData/',1)[0])+'/cfipython/slc5_amd64_gcc434/ZShape/ZFromData')
