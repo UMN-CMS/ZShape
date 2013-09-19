f775a6fb7b104d5d5a289c149ade89650a49f9e1
Alexander Gude <gude@physics.umn.edu>
2013-09-19 14:23:42 -0500

    Update run_eb_eb_and_eb_ee_fit.py
    Add in background_ele27.txt which is needed to run
    run_eb_eb_and_eb_ee_fit.py.
    
    run_eb_eb_and_eb_ee_fit.py now covers all pt bins (it missed one
    previously).
    
    run_eb_eb_and_eb_ee_fit.py makes a random temporary directory for
    plotting output.

--- ZShape/2011EffCalculation/fitEffAcc/run_eb_eb_and_eb_ee_fit.py
+++ ZShape/2011EffCalculation/fitEffAcc/run_eb_eb_and_eb_ee_fit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from subprocess import call  # Access external programs
-from os import mkdir
+from tempfile import mkdtemp  # Secure methods of generating random directories
 
 # Multiprocessing does not exist on some of the older versions of Scientific
 # Linux that we still use at CERN
@@ -18,8 +18,8 @@ else:  # Only runs when try succeeds
 
 # Input Files
 fitFile = "background_ele27.txt"
-signalFile = "/local/cms/user/gude/2012_kevin_thesis_eff/gsf_gsf_mc/gsf_gsf_mc_summed.root"
-dataFile = "/local/cms/user/gude/2012_kevin_thesis_eff/gsf_gsf_mc/gsf_gsf_mc_summed.root"
+signalFile = "/local/cms/user/gude/2012_kevin_thesis_eff/20130918_gsf_gsf_mc/20130918_gsf_gsf_mc_summed.root"
+dataFile = "/local/cms/user/gude/2012_kevin_thesis_eff/20130918_gsf_gsf_mc/20130918_gsf_gsf_mc_summed.root"
 # Mass_Z
 minMZ = 60
 maxMZ = 150
@@ -31,13 +31,13 @@ usePhiStar = False
 exe = "./fitEffAcc.exe"
 
 # Output dir
-outdir="/tmp/gsf_gsf_mc_run_as_data"
-mkdir(outdir)
+outdir = mkdtemp(prefix="gsf_gsf_mc_run_as_data_")
+print "# Output directory: ", outdir
 
 # Bins
 probeLocs = ("EB", "EEp", "EEm")
 PUs = ((0, 4), (5, 10), (11, 101))
-Xs = ((20, 25), (25, 30), (30, 35), (35, 40), (40, 45), (45, 50), (55, 60), (60, 500))
+Xs = ((20, 25), (25, 30), (30, 35), (35, 40), (40, 45), (45, 50), (50, 55), (55, 60), (60, 500))
 
 # List to store points to run over before passing to the multiprocessing pool
 inputList = []
